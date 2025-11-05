23bb28ddbaed0d8656949ab5504e48f1543f1b1e
Tor Henning Egenes <tor.henning.egenes@gmail.com>
2025-11-05 20:49:37 +0100

    feat: tweak zod schema

--- src/validation/player.ts
+++ src/validation/player.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const playerSchema = z.object({
     name: z.string().min(1),
-    faction: z.string().min(1),
+    factions: z.array(z.string().min(1)),
     notes: z.string().optional(),
 });
 
